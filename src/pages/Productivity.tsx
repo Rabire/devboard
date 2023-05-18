4145bba6ba99c7ec5d4658156d11929fb36e17e0
Rabire <rabireh@outlook.fr>
2023-05-18 14:09:43 +0200

    feat: ✨ productivity page layout

--- src/pages/Productivity.tsx
+++ src/pages/Productivity.tsx
@@ -1,3 +1,28 @@
-const Productivity = () => <main>Productivity page</main>;
+const Productivity = () => (
+  <main className="flex flex-col md:grid grid-cols-3 grid-rows-[5fr_1fr] gap-5">
+    {/* Left */}
+    <div className="card">Todolist</div>
+
+    {/* Middle */}
+    <div className="grid grid-rows-4 gap-5">
+      <div className="card">Music</div>
+
+      <div className="row-span-4 grid gap-5">
+        <div className="card">Temps par projet</div>
+        <div className="card">Tâches términées</div>
+      </div>
+    </div>
+
+    {/* Right */}
+    <div className="row-span-2 grid gap-5 grid-rows-[auto_1fr]">
+      <div className="card">
+        <p>Calendar</p>
+      </div>
+      <div className="card">Bullet journal</div>
+    </div>
+
+    <div className="card col-span-2">slider</div>
+  </main>
+);
 
 export default Productivity;
