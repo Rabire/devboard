2baedb6ff898d9af386f5c1065f07b7f658aecab
Rabire <rabireh@outlook.fr>
2023-05-18 19:24:04 +0200

    style: 🎨 TodoTask checkbok ok

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,6 +1,15 @@
+import cn from "classnames";
+
 const TodoTask = () => (
-  <div className="py-5 flex gap-2.5 border-bottom">
-    <input type="checkbox" />
+  <div className="py-5 flex gap-2.5 border-bottom items-center">
+    <input
+      type="checkbox"
+      className={cn(
+        "appearance-none h-5 aspect-square border-[1px] rounded-full border-black dark:border-light relative",
+        "checked:bg-current-theme checked:border-0",
+        "checked:after:content-[url(icons/checked-black.svg)] dark:checked:after:content-[url(icons/checked-black.svg)] checked:after:absolute checked:after:left-1/2 checked:after:-translate-x-1/2"
+      )}
+    />
     <div className="space-y-2.5">
       <p className="font-bold">TodoTask</p>
       <p>
