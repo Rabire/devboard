e05beff362db0768354b89fff094f41808059731
Rabire <rabireh@outlook.fr>
2023-05-19 23:21:10 +0200

    feat: ✨ Activity card title truncate & reveal on hover

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -14,13 +14,15 @@ const ActivityCard = ({ activities, hour }: Props) => {
   return (
     <div
       className={cn(
-        "bg-current-theme text-black text-start p-1 flex gap-5 justify-between z-10",
+        "bg-current-theme text-black text-start p-1 flex gap-5 justify-between z-10 group",
         "absolute left-2 right-0",
         isPast(activity.end) && "grayscale-75"
       )}
       style={{ height: `${duration}00%` }}
     >
-      {duration} {activity.name}
+      <p className="truncate group-hover:overflow-visible group-hover:text-clip group-hover:whitespace-normal">
+        {activity.name}
+      </p>
       <div className="text-xs bg-dark text-white py-0.5 px-5 rounded-full whitespace-nowrap self-start">
         {activity.project}
       </div>
