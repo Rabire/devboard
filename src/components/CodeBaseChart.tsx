58c30686f4a13cc63b0bb8132cc737ae9b342dc0
Rabire <rabireh@outlook.fr>
2023-05-26 07:50:11 +0200

    fix: 🐛 vode base chart change with project

--- src/components/CodeBaseChart.tsx
+++ src/components/CodeBaseChart.tsx
@@ -26,11 +26,8 @@ const OPTIONS = {
       grid: { color: THEME_COLORS[mode] },
       angleLines: { color: THEME_COLORS[mode] },
       pointLabels: {
-        // display: false,
-        // backdropPadding: 0,
         font: { size: 12 },
         color,
-        // color and font
       },
     },
   },
@@ -47,17 +44,6 @@ const LABELS = [
   "Accessibilité",
 ];
 
-const data = {
-  labels: LABELS,
-  datasets: [
-    {
-      label: "% de satisfaction",
-      data: LABELS.map(() => faker.number.int({ min: 20, max: 100 })),
-      backgroundColor: theme,
-    },
-  ],
-};
-
 ChartJS.register(
   RadialLinearScale,
   PointElement,
@@ -67,11 +53,24 @@ ChartJS.register(
   Legend
 );
 
-const CodeBaseChart = () => (
-  <section>
-    <h2>Codebase actuelle</h2>
-    <Radar options={OPTIONS} data={data} />
-  </section>
-);
+const CodeBaseChart = () => {
+  const data = {
+    labels: LABELS,
+    datasets: [
+      {
+        label: "% de satisfaction",
+        data: LABELS.map(() => faker.number.int({ min: 20, max: 100 })),
+        backgroundColor: theme,
+      },
+    ],
+  };
+
+  return (
+    <section>
+      <h2>Codebase actuelle</h2>
+      <Radar options={OPTIONS} data={data} />
+    </section>
+  );
+};
 
 export default CodeBaseChart;
