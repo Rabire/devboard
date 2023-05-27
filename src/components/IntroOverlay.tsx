b51ba743415f269393452e34086c3e767a75160a
Rabire <rabireh@outlook.fr>
2023-05-27 19:35:03 +0200

    style: 🎨 responsive overlay

--- src/components/IntroOverlay.tsx
+++ src/components/IntroOverlay.tsx
@@ -3,10 +3,16 @@ import DevBoardLabel from "./svg/DevBoardLabel";
 
 const IntroOverlay = () => (
   <div className="absolute inset-0 bg z-50  flex items-center justify-center p-5 pointer-events-none anim-intro-overlay">
-    <div className="flex flex-col items-center  gap-10 max-w-4xl">
-      <DevBoardLogo size="lg" pathColor="primary" />
+    <div className="flex flex-col items-center gap-10 max-w-4xl">
+      <div className="hidden tablet:block">
+        <DevBoardLogo size="lg" pathColor="primary" />
+      </div>
+
+      <div className="block tablet:hidden">
+        <DevBoardLogo size="md" pathColor="primary" />
+      </div>
       <DevBoardLabel />
-      <p className="font-bold text-2xl text-center">
+      <p className="font-bold text-xl tablet:text-2xl text-center">
         Boostez votre productivité freelance grâce au tableau de bord dédié aux
         codeurs passionnés !
       </p>
