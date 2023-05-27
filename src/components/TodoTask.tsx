7356a1ddfdadc285f29d20f4da356ef3075b46f5
Rabire <rabireh@outlook.fr>
2023-05-27 18:48:44 +0200

    style: 🎨 opacity on task options

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -22,7 +22,8 @@ const TodoTask = ({ task, index }: Props) => (
         className={cn(
           "appearance-none h-5 aspect-square border-[1px] rounded-full border-black dark:border-light relative",
           "checked:bg-current-theme checked:border-0",
-          "checked:after:content-[url(icons/checked-black.svg)] dark:checked:after:content-[url(icons/checked-black.svg)] checked:after:absolute checked:after:left-1/2 checked:after:-translate-x-1/2"
+          "checked:after:content-[url(icons/checked-black.svg)] dark:checked:after:content-[url(icons/checked-black.svg)]",
+          "checked:after:absolute checked:after:left-1/2 checked:after:-translate-x-1/2"
         )}
       />
       <div className="space-y-1">
@@ -30,7 +31,7 @@ const TodoTask = ({ task, index }: Props) => (
         {task.description && <p className="text-xs">{task.description}</p>}
       </div>
 
-      <div className="hidden absolute bg-secondary gap-2.5 p-2.5 right-0 top-2.5 group-hover:flex">
+      <div className="flex opacity-0 transition-opacity absolute bg-secondary gap-2.5 p-2.5 right-0 top-2.5 group-hover:opacity-100">
         <button disabled>
           <BinIcon />
         </button>
