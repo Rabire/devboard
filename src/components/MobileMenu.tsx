a134c9722df9eb75b7ad6f0cfbeb913210e6539d
Rabire <rabireh@outlook.fr>
2023-05-28 14:36:09 +0200

    feat: ✨ animate presence mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import cn from "classnames";
 import BurgerIcon from "./svg/BurgerIcon";
 import useMenuVisibility from "../hooks/useMenuVisibility";
@@ -16,48 +17,54 @@ const MobileMenu = () => {
 
   useEffect(() => setShowModal(false), [location]);
 
-  if (!isVisible) return null;
-
   // TODO: handle close on click outside
   return (
-    <aside
-      className={cn(
-        "fixed drop-shadow-xl z-50 bg-secondary flex flex-col overflow-hidden tablet:hidden",
-        !showModal &&
-          "right-3 top-3 rounded-full aspect-square border-light dark:border-dark border-2",
-        !showModal && "right-3 top-3 rounded-full aspect-square",
-        showModal && "rounded-none p-3 inset-0 left-auto",
-        showModal ? "justify-between" : "justify-center"
-      )}
-    >
-      <button
-        onClick={() => setShowModal((prev) => !prev)}
-        className="flex justify-end p-5"
-      >
-        {showModal ? <ExitCrossIcon /> : <BurgerIcon />}
-      </button>
-
-      {showModal && (
-        <>
-          <div className="flex justify-center">
-            <DevBoardLogo size="sm" pathColor="secondary" />
-          </div>
-          <div className="flex flex-col gap-3 text-2xl px-4">
-            {MENU_LINKS.map((link) => (
-              <Link key={link.link} to={link.link} className="text-center">
-                {link.text}
-              </Link>
-            ))}
-          </div>
-
-          <OptionsDialog />
-
-          <button disabled className="text-xs w-full">
-            Déconnexion
+    <AnimatePresence>
+      {isVisible && (
+        <motion.aside
+          key="mobile-menu"
+          className={cn(
+            "fixed drop-shadow-xl z-50 bg-secondary flex flex-col overflow-hidden tablet:hidden",
+            !showModal &&
+              "right-3 top-3 rounded-full aspect-square border-light dark:border-dark border-2",
+            !showModal && "right-3 top-3 rounded-full aspect-square",
+            showModal && "rounded-none p-3 inset-0 left-auto",
+            showModal ? "justify-between" : "justify-center"
+          )}
+          initial={{ x: 100 }}
+          animate={{ x: 0 }}
+          exit={{ x: 100 }}
+        >
+          <button
+            onClick={() => setShowModal((prev) => !prev)}
+            className="flex justify-end p-5"
+          >
+            {showModal ? <ExitCrossIcon /> : <BurgerIcon />}
           </button>
-        </>
+
+          {showModal && (
+            <>
+              <div className="flex justify-center">
+                <DevBoardLogo size="md" pathColor="secondary" />
+              </div>
+              <div className="flex flex-col gap-3 text-2xl px-4">
+                {MENU_LINKS.map((link) => (
+                  <Link key={link.link} to={link.link} className="text-center">
+                    {link.text}
+                  </Link>
+                ))}
+              </div>
+
+              <OptionsDialog />
+
+              <button disabled className="text-xs w-full">
+                Déconnexion
+              </button>
+            </>
+          )}
+        </motion.aside>
       )}
-    </aside>
+    </AnimatePresence>
   );
 };
 
