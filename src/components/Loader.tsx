97b81463a53f4e128def58da46c1357e9c8c17ce
Rabire <rabireh@outlook.fr>
2023-05-28 19:56:03 +0200

    style: 🎨 desktop Loader layout fixed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,9 @@
 const Loader = () => (
   <main className="desktop:grid-rows-4">
     <div className="bg-secondary rounded-2xl animate-pulse desktop:row-span-3" />
-    <div className="bg-secondary rounded-2xl animate-pulse tablet:row-span-2 desktop:col-span-2" />
+    <div className="bg-secondary rounded-2xl animate-pulse tablet:row-span-2 desktop:row-span-1 desktop:col-span-2" />
     <div className="bg-secondary rounded-2xl animate-pulse desktop:row-span-2" />
-    <div className="bg-secondary rounded-2xl animate-pulse tablet:col-span-2 desktop:row-span-2" />
+    <div className="bg-secondary rounded-2xl animate-pulse tablet:col-span-2 desktop:col-span-1 desktop:row-span-2" />
     <div className="bg-secondary rounded-2xl animate-pulse hidden desktop:block desktop:col-span-2" />
     <div className="bg-secondary rounded-2xl animate-pulse hidden desktop:block" />
   </main>
