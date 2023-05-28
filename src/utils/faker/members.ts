49abef9d437ca8434395c9cf4b653c7ac206682b
Rabire <rabireh@outlook.fr>
2023-05-28 09:14:26 +0200

    feat: ✨ static hg images

--- src/utils/faker/members.ts
+++ src/utils/faker/members.ts
@@ -1,13 +1,14 @@
-import { rand, randAvatar, randFullName, randUuid } from "@ngneat/falso";
+import { rand, randFullName, randUuid } from "@ngneat/falso";
 import { Person, Sprint } from "../types";
 import { TEAMS } from "../../static/mock-projects";
+import MOCK_AVATARS from "../../static/mock-avatars";
 
 const generatePerson = (sprints: Sprint[]): Person => ({
   id: randUuid(),
   name: randFullName(),
   team: rand(TEAMS),
   sprint: rand(sprints),
-  image: randAvatar(),
+  image: rand(MOCK_AVATARS),
 });
 
 export default generatePerson;
